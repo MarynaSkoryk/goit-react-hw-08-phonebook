1339a3224238263fd7b8d72c80d4cb5888dd5aea
Maryna Skoryk <skoryk.maryna@gmail.com>
2023-03-11 10:06:29 +0100

    Update constants-url.js

--- src/constants/constants-url.js
+++ src/constants/constants-url.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query';
 
-// const BASE_URL = 'https://connections-api.herokuapp.com';
-const BASE_URL = 'https://6404629a80d9c5c7bac69574.mockapi.io/contacts';
+const BASE_URL = 'https://connections-api.herokuapp.com';
+// const BASE_URL = 'https://6404629a80d9c5c7bac69574.mockapi.io/contacts';
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
